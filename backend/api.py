6c0d49ad133d4f66862fa0b3b30c65c0382e4a98
Lekhya <k.lekhyamvm10@gmail.com>
2026-03-05 15:41:33 +0530

    api part added successfully

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 import tensorflow as tf
 import numpy as np
+import librosa
 
 app = FastAPI()
 
+# load trained model
 model = tf.keras.models.load_model("models/cry_model.h5")
 
 classes = [
@@ -22,9 +24,16 @@ def home():
     return {"message": "Infant Cry Analyzer API running"}
 
 @app.post("/predict")
-def predict(features: list):
+async def predict(file: UploadFile = File(...)):
 
-    features = np.array(features).reshape(1,-1)
+    # load audio
+    audio, sr = librosa.load(file.file, sr=22050)
+
+    # extract MFCC features
+    mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=40)
+    features = np.mean(mfcc.T, axis=0)
+
+    features = features.reshape(1, -1)
 
     prediction = model.predict(features)
 
